d867bc0f9be66f36c6c6f5174d10322ba9765ddc
Toxin4426 <Toxin4426@gmail.com>
2024-09-04 18:28:27 -0400

    simplifed code

--- src/chocobot/app.py
+++ src/chocobot/app.py
@@ -26,17 +26,16 @@ async def on_message(message: discord.Message) -> None:
     if message.author == bot.user:
         return
     if any(role.name == 'admin' for role in message.author.roles):
-        await message.channel.send(f"{message.author.mention}, is an admin")
         await bot.process_commands(message)
         return
-    else:
-    # Prevent spam (2 or more identical messages in a row)
-        messages: list[discord.Message] = [msg async for msg in message.channel.history(limit=2)]
-        if all(msg.content == message.content for msg in messages):
-            await message.delete()
-            await message.channel.send(f"{message.author.mention}, please don't spam!")
 
-   # await bot.process_commands(message)
+    # Prevent spam (2 or more identical messages in a row)
+    messages: list[discord.Message] = [msg async for msg in message.channel.history(limit=2)]
+    if all(msg.content == message.content for msg in messages):
+        await message.delete()
+        await message.channel.send(f"{message.author.mention}, please don't spam!")
+
+    await bot.process_commands(message)
 
 @bot.command()
 async def hello(ctx: commands.Context[Any]) -> None:
